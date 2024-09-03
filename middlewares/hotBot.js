f3308ebadc8ec36b8644fd4490b6d86818bb94a6
Uttam Verma <imuv21@gmail.com>
2024-09-03 12:23:58 +0530

    update

--- middlewares/hotBot.js
+++ middlewares/hotBot.js
@@ -1,33 +1,33 @@
-import { Client, GatewayIntentBits } from 'discord.js';
-import { REST, Routes } from 'discord.js';
+// import { Client, GatewayIntentBits } from 'discord.js';
+// import { REST, Routes } from 'discord.js';
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+// const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
-client.on('messageCreate', (message) => {
-    if (message.author.bot) return;
-    message.reply({
-        content: `Hi ${message.author.username}, how are you?`
-    });
-});
+// client.on('messageCreate', (message) => {
+//     if (message.author.bot) return;
+//     message.reply({
+//         content: `Hi ${message.author.username}, how are you?`
+//     });
+// });
 
-//HotBot
-client.login(process.env.BOT_TOKEN);
+// //HotBot
+// client.login(process.env.BOT_TOKEN);
 
-const commands = [
-    {
-        name: 'ping',
-        discription: 'dgkdlg'
-    },
-];
+// const commands = [
+//     {
+//         name: 'ping',
+//         discription: 'dgkdlg'
+//     },
+// ];
 
-const rest = new REST({ version: "10" }).setToken(process.env.BOT_TOKEN);
+// const rest = new REST({ version: "10" }).setToken(process.env.BOT_TOKEN);
 
-(async () => {
-    try {
-        console.log('Started refreshing application (/) commands.');
-        await rest.put(Routes.applicationCommands(process.env.BOT_CLIENT_ID), { body: commands });
-        console.log('Successfully reloaded application (/) commands.');
-    } catch (error) {
-        console.error(error);
-    }
-})();
+// (async () => {
+//     try {
+//         console.log('Started refreshing application (/) commands.');
+//         await rest.put(Routes.applicationCommands(process.env.BOT_CLIENT_ID), { body: commands });
+//         console.log('Successfully reloaded application (/) commands.');
+//     } catch (error) {
+//         console.error(error);
+//     }
+// })();
