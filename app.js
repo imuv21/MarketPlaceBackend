b5343e3c62dad8840b4bdfc1728e790f10eeff46
Uttam Verma <imuv21@gmail.com>
2024-09-11 13:44:30 +0530

    rate-limit

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(cors({
 // Rate Limiting
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100 // limit each IP to 100 requests per windowMs
+    max: 450 // limit each IP to 450 requests per windowMs
 });
 app.use(limiter);
 
